11825d04c857c736e5441b665d579aac5b97b3af
Connor Cimowsky <connorcimowsky@me.com>
2015-05-27 16:57:13 -0400

    Add code for generating exponentially distributed random variables

---
+++ lab1/ExponentialDistribution.java
@@ -0,0 +1,6 @@
+public class ExponentialDistribution {
+    public static double randomVariable(double lambda) {
+        double uniformRandomVariable = Math.random();
+        return Math.log(1 - uniformRandomVariable) / (-lambda);
+    }
+}
